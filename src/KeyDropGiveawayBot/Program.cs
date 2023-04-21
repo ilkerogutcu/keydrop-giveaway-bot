afddd207fa415dbc2ecd11cdb18fbd6a46c3cc20
ilkerogutcu <ilkerogtc@gmail.com>
2023-04-21 19:12:45 +0300

    feat: add serilog configuration

--- src/KeyDropGiveawayBot/Program.cs
+++ src/KeyDropGiveawayBot/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Figgle;
+using KeyDropGiveawayBot.Utils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
-Console.WriteLine("Hello, World!");
+var builder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", false, true)
+    .AddEnvironmentVariables();
+var configuration = builder.Build();
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console(outputTemplate: "[{Timestamp:y-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+    .WriteTo.File($"logs\\log-.txt", rollingInterval: RollingInterval.Day,
+        outputTemplate: "[{Timestamp:y-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+    .CreateLogger();
+
+using var host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) =>
+    {
+        services.AddScoped<IApiClient, ApiClient>();
+        services.AddSingleton(context.Configuration);
+    })
+    .ConfigureAppConfiguration((context, config) => { config.AddJsonFile("appsettings.json", false, true); })
+    .Build();
+
+
+Console.WriteLine(FiggleFonts.Standard.Render("KeyDrop Giveaway Bot"));
+Log.Information("Starting KeyDrop Giveaway Bot");
+Log.CloseAndFlush();
